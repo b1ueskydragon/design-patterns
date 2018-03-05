cdddbb8f05395b9264af09d035ce9791d6204ffc
jang <jang_inhwa@microad.co.jp>
2018-03-05 19:12:26 +0900

    Optionaaaaal

---
+++ HelloHi/src/Optionaaal.scala
@@ -0,0 +1,14 @@
+object Optionaaal {
+  val naaame =
+    Map("Me" -> "Aleeex", "You" -> "Rick")
+
+  def show(x: Option[String]) = x match {
+    case Some(s) => s
+    case None => "?"
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(show(naaame get "Me"))
+    println(show(naaame get "She"))
+  }
+}
