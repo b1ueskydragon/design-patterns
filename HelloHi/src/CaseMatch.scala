f77f9bbdd0810f4e023fd221dcfd70c510b78e72
jang <jang_inhwa@microad.co.jp>
2018-03-27 17:13:48 +0900

    Be smart

--- HelloHi/src/CaseMatch.scala
+++ HelloHi/src/CaseMatch.scala
@@ -4,9 +4,15 @@ object CaseMatch {
     case Some(_) => Some("0")
   }
 
+  private def flagMap(isTrue: Option[Boolean]): Option[Int] = isTrue.map(f => if (f) 1 else 0)
+
   def main(args: Array[String]): Unit = {
     println(flag(Some(true)))
     println(flag(Some(false)))
-    println(flag(None)) // case None 処理せずに None が return されるので例外.
+  //  println(flag(None)) // case None 処理せずに None が return されるので例外.
+
+    println(flagMap(Some(true)))
+    println(flagMap(Some(false)))
+    println(flagMap(None))
   }
 }
