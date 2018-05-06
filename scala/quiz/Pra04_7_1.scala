fbc8926bc345a6f064b5fb246ff230410dcf599a
inagon <dragoalie@gmail.com>
2018-05-06 23:57:11 +0900

    Use seq

--- scala/quiz/Pra04_7_1.scala
+++ scala/quiz/Pra04_7_1.scala
@@ -3,7 +3,7 @@ package quiz
 // forを用いて 1! から 10! までを要素とするリスト(あるいはVector)を求めよ．
 object Pra04_7_1 {
   def main(args: Array[String]): Unit = {
-    println(comprehension(1 to 10 toList))
+    println(comprehension(1 to 10))
   }
 
   def factorial(n: Int): Int = {
@@ -16,7 +16,7 @@ object Pra04_7_1 {
     _tailRec(1, n)
   }
 
-  def comprehension(nums: List[Int]): List[Int] = {
+  def comprehension(nums: Seq[Int]): Seq[Int] = {
     for {
       num <- nums
     } yield factorial(num)
