2a9d398c9836c2f2fb27bebdb19e1c33111d78c2
inagon <dragoalie@gmail.com>
2018-05-13 23:33:02 +0900

    Type double result

---
+++ scala/quiz/Pra04_3_10.scala
@@ -0,0 +1,17 @@
+package quiz
+
+// n=100 の時， ∑n i=1 1/i を計算せよ
+object Pra04_3_10 {
+  def main(args: Array[String]): Unit = {
+    val n = 100
+
+    val res = (1 to n).map(1d / _).sum
+
+    val res01 = (for {
+      i <- Range(1, n + 1)
+    } yield 1.0 / i).sum
+
+    println(res)
+    println(res01)
+  }
+}
