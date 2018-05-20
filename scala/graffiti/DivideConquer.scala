20f87b2e65933144f100cec2a11025a6f9f66262
inagon <dragoalie@gmail.com>
2018-05-20 18:23:33 +0900

    Design cases first

---
+++ scala/graffiti/DivideConquer.scala
@@ -0,0 +1,12 @@
+package graffiti
+
+object DivideConquer {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def append[A](xs: List[A], ys: List[A]): List[A] = xs match {
+    case List() => ???
+    case h :: tail => ???
+  }
+}
