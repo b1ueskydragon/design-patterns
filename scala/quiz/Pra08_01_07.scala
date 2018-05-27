64b62b36a0f185edd135cacf7536beeb3e387c82
jang <jang_inhwa@microad.co.jp>
2018-05-28 01:12:46 +0900

    OK

--- scala/quiz/Pra08_01_07.scala
+++ scala/quiz/Pra08_01_07.scala
@@ -5,12 +5,15 @@ object Pra08_01_07 {
   def main(args: Array[String]): Unit = {
     val matrix = List(List(3, 1, 4, 1), List(2, 7, 1, 8), List(0, 5, 7, 7))
     // (3, 2, 0) (1, 7, 5) (4, 1, 7) (1, 8, 7)
-  }
 
-  // a element of matrix.
-  def extract(res: List[List[Int]], list: List[Int]): List[List[Int]] = list match {
-    case e if e.isEmpty => res
-    case h :: tail => extract(res ::: List(List(h)), tail)
-  }
+//    val res = matrix map {
+//      _.head
+//    }
 
+    val res = for {
+      idx <- matrix.head.indices
+    } yield matrix.map(_ (idx))
+
+    println(res)
+  }
 }
