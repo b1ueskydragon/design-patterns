fc4963e233ff4900224022cda101efbe15ef0ef1
jang <jang_inhwa@microad.co.jp>
2018-05-27 15:27:07 +0900

    WIP

---
+++ scala/quiz/Pra08_01_07.scala
@@ -0,0 +1,12 @@
+package quiz
+
+// 行列をリストのリストで表す.
+// 変数 matrix が List(List(3,1,4,1), List(2,7,1,8), List(0,5,7,7)) である時，
+// 転置行列(transposed matrix)を求めるにはどうすれば良いか．
+object Pra08_01_07 {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
