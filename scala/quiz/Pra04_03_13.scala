0eb11f4e74c3f75d600b558cc65cb07865ed29f0
jang <jang_inhwa@microad.co.jp>
2018-05-31 11:39:43 +0900

    Refactor + wip

--- scala/quiz/Pra04_03_13.scala
+++ scala/quiz/Pra04_03_13.scala
@@ -14,28 +14,45 @@ object Pra04_03_13 {
 
     println(res)
 
-    // TODO
-    println((20 to 0 by -1).map(n => (1 to n).map(1.0 / _).product))
+    lazy val sample = (20 to 0 by -1) map { n =>
+      (1 to n).map {
+        1.0 / _
+      }.product
+    }
+
+    println(sample)
+
+    lazy val inner = (1 to n).map {
+      1.0 / _
+    }.product
+
+    println(inner)
   }
 
   def product[T: Numeric](n: T): T = {
+    val _i = implicitly[Numeric[T]]
+    lazy val _0 = _i.zero
+    lazy val _1 = _i.one
+
     @tailrec
     def _tRec(n: T, res: T): T = n match {
-      case _ if n == 0 => implicitly[Numeric[T]].zero
-      case _ if n == 1 => res
-      case _ => _tRec(implicitly[Numeric[T]].minus(n, implicitly[Numeric[T]].one), implicitly[Numeric[T]].times(res, n))
+      case _ if n == _0 => _0
+      case _ if n == _1 => res
+      case _ => _tRec(_i.minus(n, _1), _i.times(res, n))
     }
 
-    _tRec(n, implicitly[Numeric[T]].one)
+    _tRec(n, _1)
   }
 
   def sum[T: Numeric](list: List[T]): T = {
+    lazy val _i = implicitly[Numeric[T]]
+
     @tailrec
     def _tRec(list: List[T], res: T): T = list match {
       case Nil => res
-      case h :: tail => _tRec(tail, implicitly[Numeric[T]].plus(res, h))
+      case h :: tail => _tRec(tail, _i.plus(res, h))
     }
 
-    _tRec(list, implicitly[Numeric[T]].zero)
+    _tRec(list, _i.zero)
   }
 }
