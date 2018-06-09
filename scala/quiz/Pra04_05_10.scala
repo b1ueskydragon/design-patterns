114caa01f1bb9b765002c3bcfa1b1bade3280e60
jang <jang_inhwa@microad.co.jp>
2018-06-10 01:03:58 +0900

    Init approach wt recursion

--- scala/quiz/Pra04_05_10.scala
+++ scala/quiz/Pra04_05_10.scala
@@ -1,11 +1,16 @@
 package quiz
 
 // 整数のリスト a0, a1, a2, …, an で，正則連分数を表すことにする.
+// a0 + ( 1 / (a1 + (1 / a2)))
 // 正則連分数を表すリストから，その実数値を求める関数cfracを定義せよ.
 // ヒント: 与えられた整数リストに対し，まず map(_.toDouble) で実数のリストに変換しておく
 object Pra04_05_10 {
   def main(args: Array[String]): Unit = {
-    lazy val list = List(1, 3, 5, 7, 9, 11)
+    lazy val from = 1
+    lazy val to = 4
+    lazy val step = 1
+    lazy val list = (from to to by step).toList
+    println(list)
     println(cfrac(list))
   }
 
@@ -15,7 +20,12 @@ object Pra04_05_10 {
     * @param list list of Integer
     * @return list of regular continued fraction
     */
-  def cfrac(list: List[Int]): List[Double] = {
-    list.map(_.toDouble)
+  def cfrac(list: List[Int]): Double = {
+    def _rec(list: List[Double]): Double = list match {
+      case Nil => Double.PositiveInfinity
+      case h :: tail => h + (1.0 / _rec(tail))
+    }
+
+    _rec(list.map(_.toDouble))
   }
 }
