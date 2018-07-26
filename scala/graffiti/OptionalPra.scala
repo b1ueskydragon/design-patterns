4f62c98bb1eef4527a5c633927cd295fa9965c97
jang <jang_inhwa@microad.co.jp>
2018-07-26 18:55:52 +0900

    isDefined

--- scala/graffiti/OptionalPra.scala
+++ scala/graffiti/OptionalPra.scala
@@ -4,7 +4,7 @@ object OptionalPra {
   val naaame =
     Map("Me" -> "Aleeex", "You" -> "Rick")
 
-  def show(x: Option[String]) = x match {
+  def show(x: Option[String]): String = x match {
     case Some(s) => s
     case None => "?"
   }
@@ -12,5 +12,10 @@ object OptionalPra {
   def main(args: Array[String]): Unit = {
     println(show(naaame get "Me"))
     println(show(naaame get "She"))
+
+    lazy val some = Some('a)
+    println(some.isDefined)
+    lazy val none = None
+    println(none.isDefined)
   }
 }
