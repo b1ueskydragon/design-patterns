f8eaaf284b1838e6610e793b6810662db6fe2783
jang <jang_inhwa@microad.co.jp>
2018-08-22 12:47:02 +0900

    WIP

--- scala/graffiti/Closures01.scala
+++ scala/graffiti/Closures01.scala
@@ -1,24 +1,14 @@
 package graffiti
 
-//object Closures01 {
-//
-//  def cons[T, U, F](a: T, b: U) = () => (f: (T, U) => F) => f(a, b)
-//
-//  def car[T, U](pair: () => T => T): T = pair((f: (T, U) => T) => a)
-//
-//  def cdr[T, U](pair: () => Pair[T, U]): U = pair.apply().u
-//
-//  def main(args: Array[String]): Unit = {
-//    val pair = cons('a, 'b)
-//    val pairPair = cons(cons(0, 1), cons(99, 100))
-//    println(car(pair))
-//    println(cdr(pair))
-//    println(cdr(car(pairPair)))
-//  }
-//}
+object Closures01 {
 
-//def cons[T, U, F](a: T, b: U) = (p: (T, U) => F) => p(a, b)
+  def cons[T, U, F](a: T, b: U) = (p: (T, U) => F) => p(a, b)
 
-//def car[T, U](f: (T, U) => T => T) = f((a: T, b: U) => a)
+  //def car[T, U](f: (T, U) => T => T) = f((a: T, b: U) => a)
 
-//def cdr[T, U](f: (T, U) => U => U) = f((a: T, b: U) => b)
+  //def cdr[T, U](f: (T, U) => U => U) = f((a: T, b: U) => b)
+
+  def main(args: Array[String]): Unit = {
+    val pair = cons('a, 'b)
+  }
+}
