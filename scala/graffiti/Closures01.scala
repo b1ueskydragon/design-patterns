264fec04bdfee56c958dea59af1e06aec2013572
jang <jang_inhwa@microad.co.jp>
2018-08-22 13:12:24 +0900

    WIP

--- scala/graffiti/Closures01.scala
+++ scala/graffiti/Closures01.scala
@@ -2,13 +2,20 @@ package graffiti
 
 object Closures01 {
 
-  def cons[T, U, F](a: T, b: U) = (p: (T, U) => F) => p(a, b)
+  def cons[T, U](a: T, b: U) = (p: (T, U) => _) => p(a, b)
 
-  //def car[T, U](f: (T, U) => T => T) = f((a: T, b: U) => a)
+  def car[T, U](p: ((T, U) => _) => _) = p((a: T, b: U) => a)
 
-  //def cdr[T, U](f: (T, U) => U => U) = f((a: T, b: U) => b)
+  def cdr[T, U](p: ((T, U) => _) => _)  = p((a: T, b: U) => b)
 
   def main(args: Array[String]): Unit = {
     val pair = cons('a, 'b)
+    val pairPair = cons(cons(0, 1), cons('i, 'j))
+    println(car(pair))
+    println(cdr(pair))
+
+    val test = cdr(pairPair)
+    // car(cdr(pairPair)) // TODO
+    println(test)
   }
 }
