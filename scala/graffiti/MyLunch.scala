9925f8a3a673d8f29b74fb3f8bff0fcd7501e956
jang <jang_inhwa@microad.co.jp>
2018-10-18 11:56:05 +0900

    WIP

--- scala/graffiti/MyLunch.scala
+++ scala/graffiti/MyLunch.scala
@@ -5,12 +5,27 @@ import scala.util.Random
 
 object MyLunch {
   def main(args: Array[String]): Unit = {
-    println(selectLunch(candidates().take(3)))
-    // TODO take a weight for select randomly
+//    println(selectLunch(candidates().take(3))) // Enroll 3 menus.
+    val input = List(("a", 30), ("b", 10), ("c", 40), ("d", 20))
+    println(selectLunch(input))
+
   }
 
-  def selectLunch(candidates: Stream[(String, Int)]): List[(String, Int)] = {
-    Random.shuffle(candidates).take(1).toList
+  /**
+    * Weighted random number.
+    *
+    * String: menu
+    * Int: weight
+    */
+  def selectLunch(candidates: List[(String, Int)]): List[String] = {
+    // total weight.
+    val total = candidates.map(_._2).sum
+
+    // random weight.
+    // pick a value randomly from a range 1 to total.
+    val random = Random.nextInt(total) + 1
+
+    candidates.filter(_._2 >= random).map(_._1)
   }
 
   private def candidate(): (String, Int) = {
