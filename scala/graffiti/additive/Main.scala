f2ffd6e22c0c05d86867d20950b5f2a283254946
jang <dragoalie@gmail.com>
2018-10-20 10:58:23 +0900

    Add more usage eg from std lib

--- scala/graffiti/additive/Main.scala
+++ scala/graffiti/additive/Main.scala
@@ -7,10 +7,12 @@ object Main {
   def sum[A](lst: List[A])(implicit m: Additive[A]): A = lst.foldLeft(m.zero)((x, y) => m.plus(x, y))
 
   def main(args: Array[String]): Unit = {
-
-    println("型チェックする時点で List の型が伝わっているため, 型推論を利用する")
-
+    // 型チェックする時点で List の型が伝わっているため, 型推論を利用する
     println(sum(List(1, 2, 3)))
     println(sum(List("A", "B", "C")))
+
+    // A sample usage from Standard library
+    println(List[Int]().sum)
+    println(List(1.1, 1.2, 1.3, 1.4).sum)
   }
 }
