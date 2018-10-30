4b379e8dd17761e7cd3e312f1667bfd988485889
jang <jang_inhwa@microad.co.jp>
2018-10-30 19:36:35 +0900

    Add an bad efficiency example using Iterable instead of Traversable

--- scala/graffiti/Tree.scala
+++ scala/graffiti/Tree.scala
@@ -1,6 +1,12 @@
 package graffiti
 
-// Traversable means `たどれる`
+/** Traversable means `たどれる`
+  *
+  * N (葉の数)
+  * N - 1 (Branch クラスの内部ノードの数)
+  *
+  * N + N - 1 (木の要素の全探索に必要な step 数)
+  */
 sealed abstract class Tree extends Traversable[Int] {
   // override
   def foreach[U](f: Int => U): Unit = this match {
@@ -11,8 +17,20 @@ sealed abstract class Tree extends Traversable[Int] {
   }
 }
 
+/** `++` で l.iterator ++ r.iterator を作るたびに,
+  * l.iterator または r.iterator を得ることとなる.
+  *
+  * そのため, 1つの葉にたどりつくために log(N) 回の関節処理(step)が発生する.
+  *
+  * N log(N)
+  */
+//sealed abstract class Tree extends Iterable[Int] {
+//  def iterator: Iterator[Int] = this match {
+//    case Node(elem) => Iterator.single(elem)
+//    case Branch(l, r) => l.iterator ++ r.iterator
+//  }
+//}
+
 case class Branch(left: Tree, right: Tree) extends Tree
 
 case class Node(elem: Int) extends Tree
-
-
