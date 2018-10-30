2ed514372a267096b1aee40374db1730efeaa7af
jang <jang_inhwa@microad.co.jp>
2018-10-30 19:19:58 +0900

    Traversable Tree structure

--- scala/graffiti/Tree.scala
+++ scala/graffiti/Tree.scala
@@ -1,7 +1,18 @@
 package graffiti
 
-sealed abstract class Tree
+// Traversable means `たどれる`
+sealed abstract class Tree extends Traversable[Int] {
+  // override
+  def foreach[U](f: Int => U): Unit = this match {
+    case Node(elem) => f(elem)
+    case Branch(l, r) =>
+      l foreach f
+      r foreach f
+  }
+}
 
 case class Branch(left: Tree, right: Tree) extends Tree
 
 case class Node(elem: Int) extends Tree
+
+
