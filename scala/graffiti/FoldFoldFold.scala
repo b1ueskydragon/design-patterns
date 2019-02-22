59d4e0d4a16c2c7bd48e4ec61b8b4f3b67e8fd3f
jang <dragoalie@gmail.com>
2019-02-22 10:12:46 +0900

    foldleft, tailrec

---
+++ scala/graffiti/FoldFoldFold.scala
@@ -0,0 +1,12 @@
+package graffiti
+
+import scala.annotation.tailrec
+
+class FoldFoldFold {
+
+  //  // List[A]
+  //  @tailrec def foldLeft[B](zero: B)(f: (B, A) => B): B =
+  //    if (this.isEmpty) zero
+  //    else tail.foldLeft(f(zero, head))(f)
+
+}
