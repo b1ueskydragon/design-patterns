841c3eaa66877bfc835fd8f0df9a7f72c1e75b03
jang <dragoalie@gmail.com>
2019-05-23 20:15:51 +0900

    Create using patterns for file

---
+++ src/main/scala/designPatterns/loan/UsingPattern.scala
@@ -0,0 +1,34 @@
+package designPatterns.loan
+
+import java.io.File
+import java.nio.file.{Files, Path}
+
+import scala.io.Source
+
+class UsingPattern {
+
+  def using(s: Source)(f: Source => Unit): Unit = {
+    try f(s)
+    finally s.close
+  }
+
+  def fileWAndRm(content: Array[Byte], path: Path)(f2: File => Unit): Unit = {
+    val _path = Files.write(path, content)
+    f2(_path.toFile)
+    Files.deleteIfExists(_path)
+  }
+
+  def fileUsing(f1: => File)(f2: File => Unit, ef: Exception => Unit = _ => ()): Unit = {
+
+    val file = f1
+    try {
+      f2(file)
+    } catch {
+      case e: Exception => ef(e)
+    } finally {
+      Files.deleteIfExists(file.toPath)
+    }
+
+  }
+
+}
